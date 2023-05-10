75f5bdca5f94c27dafee8f7fbfdc5a2be16e4023
Aufaasadatsal <aufaasadatsalfikrisophianto>
2023-05-10 09:15:56 +0700

    Php-Mengulang

---
+++ operatorAritmatika.php
@@ -0,0 +1,48 @@
+<?php
+
+$a = 5;
+$b = 2;
+
+echo "Penjumlahan";
+echo "<br>";
+$c = $a + $b;
+echo "$a + $b = $c";
+echo "<hr>";
+
+$d = 10;
+$f = 5;
+
+echo "Pengurangan";
+echo "<br>";
+$c = $d - $f;
+echo "$d - $f = $c";
+echo "<hr>";
+
+$s = 6;
+$h = 8;
+
+echo "Perkalian";
+echo "<br>";
+$c = $s * $h;
+echo "$s * $h = $c";
+echo "<hr>";
+
+echo "Pembagian";
+echo "<br>";
+$c = $a / $b;
+echo "$a / $b = $c";
+echo "<hr>";
+
+echo "Modulus";
+echo "<br>";
+$c = $a % $b;
+echo "$a % $b = $c";
+echo "<hr>";
+
+echo "Pangkat";
+echo "<br>";
+$c = $a ** $b;
+echo "$a ** $b = $c";
+echo "<hr>";
+
+?>
