fcd795618f38c4bc3fbedd0b4386dc7b9002117c
Aufaasadatsal <aufaasadatsalfikrisophianto>
2023-05-10 13:33:13 +0700

    Php-Mengulang

---
+++ StudiKasus/Tugas _1.php
@@ -0,0 +1,34 @@
+<?php
+echo "<h1>Pembanding<h1>";
+
+
+echo "<h3>Lebih Besar<h3>";
+echo "100 > 50"; echo "<br>";
+var_export(100 > 50); echo "<br>";
+echo "<hr>";
+
+echo "<h3>Lebih Kecil<h3>";
+echo "50 < 100"; echo "<br>";
+var_export(50 < 100); echo "<br>";
+echo "<hr>";
+
+echo "<h3>Sama Dengan<h3>";
+echo "50 == 50"; echo "<br>";
+var_export(50 == 50); echo "<br>";
+echo "<hr>";
+
+echo "<h3>Tidak sama dengan<h3>";
+echo "50 != 50"; echo "<br>";
+var_export(50 != 50); echo "<br>";
+echo "<hr>";
+
+echo "<h3>Lebih besar sam dengan<h3>";
+echo "50 >= 100"; echo "<br>";
+var_export(50 >= 100); echo "<br>";
+echo "<hr>";
+
+echo "<h3>Lebih kecil sama dengan<h3>";
+echo "100 <= 50"; echo "<br>";
+var_export(100 <= 50); echo "<br>";
+echo "<hr>";
+?>
---
+++ operatorPembanding.php
@@ -0,0 +1,48 @@
+<?php
+
+$a = 10;
+$b = 4;
+
+echo "menggunakan operator relasi lebih besar";
+echo "<br>";
+$c = $a > $b;
+echo "$a > $b: $c";
+echo "<hr>";
+
+echo "menggunakan operator relasi lebih kecil";
+echo "<br>";
+$c = $a < $b;
+echo "$a < $b: $c";
+echo "<hr>";
+
+1 > 10;
+
+$A = "21";
+$B = 21;
+$A === $B;
+
+echo "menggunakan operator relasi lebih sama dengan";
+echo "<br>";
+$c = $a == $b;
+echo "$a == $b: $c";
+echo "<hr>";
+
+echo "menggunakan operator relasi lebih tidak sama dengan";
+echo "<br>";
+$c = $a != $b;
+echo "$a != $b: $c";
+echo "<hr>";
+
+echo"menggunakan operator relasi lebih besar sama dengan";
+echo "<br>";
+$c = $a >= $b;
+echo "$a >= $b: $c";
+echo "<hr>";
+
+echo "menggunakan operator relasi lebih kecil sama dengan";
+echo "<br>";
+$c = $a <= $b;
+echo "$a <= $b: $c";
+echo "<hr>";
+
+?>
